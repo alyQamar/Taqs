2a03decd24347792c31a03219330bdf18d9cd3e8
alyQamar <alyeqee@gmail.com>
2023-02-13 17:34:27 +0200

    initial commit

---
+++ server/server.js
@@ -0,0 +1,41 @@
+// Define the port and host for the server
+const port = 5000;
+const host = 'localhost';
+
+// Create the base URL for the server
+const serverUrl = `http://${host}:${port}/`;
+
+// Initialize an object to store data
+let data = {};
+
+// Import the required packages
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// Use the middleware for CORS and body parsing
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Serve static files from the "view" directory
+app.use(express.static('view'));
+
+// Define a function to handle posting data
+const posting = (req, res) => {
+  // Update the data object with the request body
+  data = req.body;
+
+  // Send a success response with the data
+  res.status(200).send(data);
+};
+
+// Define a GET route to retrieve all data
+app.get('/all', (req, res) => res.status(200).send(data));
+
+// Define a POST route to add new data
+app.post('/add', posting);
+
+// Start the server
+app.listen(port, () => console.log(`Server is running on ${serverUrl}`));
---
+++ view/app.js
@@ -0,0 +1,88 @@
+let date = new Date();
+let newDate = date.toDateString();
+
+const API_KEY = '75cad9f1db86a3c289892c67eaa91749';
+const serverUrl = 'http://localhost:5000';
+const err = document.getElementById('error');
+
+const getWeatherData = async function (zip) {
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${API_KEY}`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    if (data.cod !== 200) {
+      throw new Error(`Error: ${data.message}`);
+    }
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
+
+const postData = async function (url = '', info = {}) {
+  const newResponse = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(info),
+  });
+
+  try {
+    const savedData = await newResponse.json();
+    console.log('saved done😊 :');
+    console.log(savedData);
+    return savedData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+const updateWeather = async function () {
+  const newResponse = await fetch(serverUrl + '/all');
+  try {
+    const savedData = await newResponse.json();
+
+    document.getElementById('date').innerHTML = savedData.newDate;
+    document.getElementById('city').innerHTML = savedData.city;
+    document.getElementById('temperature').innerHTML =
+      savedData.temperature + '&degC';
+    document.getElementById('description').innerHTML = savedData.description;
+    document.getElementById('status').innerHTML = savedData.feelings;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const generateData = function () {
+  const zip = document.getElementById('zip').value;
+  const feelings = document.getElementById('feelings').value;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},
+  &appid=${API_KEY}&units=metric`;
+
+  getWeatherData(zip).then(data => {
+    if (data) {
+      const {
+        main: { temp },
+        name: city,
+        weather: [{ description }],
+      } = data;
+
+      const info = {
+        newDate,
+        city,
+        temperature: Math.round(temp),
+        description,
+        feelings,
+      };
+
+      postData(serverUrl + '/add', info);
+
+      updateWeather();
+    }
+  });
+};
+
+document.getElementById('generate').addEventListener('click', generateData);
