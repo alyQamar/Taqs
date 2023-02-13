8ecd2c635c428f308b0bf84f8344b4bfec27f080
alyQamar <alyeqee@gmail.com>
2023-02-13 18:50:09 +0200

    Convert temperature degree to metric system

--- view/app.js
+++ view/app.js
@@ -7,7 +7,7 @@ const err = document.getElementById('error');
 
 const getWeatherData = async function (zip) {
   try {
-    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${API_KEY}&units=metric`;
     const res = await fetch(url);
     const data = await res.json();
 
